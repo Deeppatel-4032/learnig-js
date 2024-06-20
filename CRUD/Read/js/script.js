ef0aaa660f1e140b75fdb7e40d45ed1ede1a758a
Deeppatel-4032 <deeppatel7868@gmail.com>
2024-06-20 15:08:09 +0530

    crud in update

--- CRUD/Read/js/script.js
+++ CRUD/Read/js/script.js
@@ -1,51 +1,137 @@
-let name = document.getElementById("fname");
+  let name = document.getElementById("fname");
 let lname = document.getElementById("lname");
 let email = document.getElementById("uEmal");
 let age = document.getElementById("age");
 let course = document.getElementById("course");
 let recod = document.getElementById("ViweData");
 
+// Error
+let errname = document.getElementById("errname");
+let errlname = document.getElementById("errlname");
+let erremail = document.getElementById("erremail");
+let errage = document.getElementById("errage");
+let errCourse = document.getElementById("errCourse");
+
+let isEdit = false;
+let isIndex;
+
+// getData
 const getData = () => {
+  let data = JSON.parse(localStorage.getItem("student_Data"));
 
-    let data = JSON.parse(localStorage.getItem("student_Data"));
-
-    if(data) {
-        return data;
-    } else {
-        return [];
-    }
-}
+  if (data) {
+    return data;
+  } else {
+    return [];
+  }
+};
 
 let Storage = getData();
 
+//create Data
 const DataFrom = () => {
   event.preventDefault();
-  let obj = {
-    id: Math.floor(Math.random() * 1000),
-    name: name.value,
-    lname: lname.value,
-    email: email.value,
-    age: age.value,
-    course: course.value
-  };
+  if (isEdit) {
+    
+    let data = [...Storage];
 
-    // StorageArray = Storage.push(obj);
-   Storage = [...Storage, obj];
+    let obj = {
+      id: isIndex ? isIndex : Math.floor(Math.random() * 1000),
+      name: name.value,
+      lname: lname.value,
+      email: email.value,
+      age: age.value,
+      course: course.value,
+    };
+    // updateData
+    const updateData = data.map((recod) => {
 
-   console.log("Storage is",Storage);
-//    console.log("obj is", obj);
+      if (recod.id == isIndex) {
+        return recod = obj;
+      } else {
+        return recod;
+      }
+    });
 
-  addData();
+    console.log("update", updateData);
+    Storage = updateData;
 
-    localStorage.setItem("student_Data",JSON.stringify(Storage));
-    name.value = "";
-    lname.value ="";
-    email.value = "";
-    age.value = "";
-    course.value = "";  
+    isIndex = undefined;
+    isEdit = false;
+  } else {
+    if (name.value === "") {
+      errname.innerHTML = "Plase Enter The First name";
+    }
+    if (lname.value === "") {
+      errlname.innerHTML = "Plase Enter The last name";
+    }
+    if (email.value === "") {
+      erremail.innerHTML = "Plase Enter The Email";
+    }
+    if (age.value === "") {
+      errage.innerHTML = "Plase Enter The Age";
+    }
+    if (course.value === "") {
+      errCourse.innerHTML = "Plase Enter the your course";
+    }
+
+    let obj = {
+      id: isIndex ? isIndex : Math.floor(Math.random() * 1000),
+      name: name.value,
+      lname: lname.value,
+      email: email.value,
+      age: age.value,
+      course: course.value,
+    };
+
+    Storage = [...Storage, obj];
+  }
+  displyData();
+
+  localStorage.setItem("student_Data", JSON.stringify(Storage));
+  name.value = "";
+  lname.value = "";
+  email.value = "";
+  age.value = "";
+  course.value = "";
 };
 
-const addData = () => {
+// Edit
+const singleRec = (id) => {
+  console.log("id", id);
+
+  let data = [...Storage];
+
+  let singleRec = data.filter((d) => {
+    return d.id == id;
+  });
+  console.log("Edit ID", singleRec[0]);
+
+  name.value = singleRec[0].name;
+  lname.value = singleRec[0].lname;
+  email.value = singleRec[0].email;
+  age.value = singleRec[0].age;
+  course.value = singleRec[0].course;
+  localStorage.setItem("student_Data", JSON.stringify(data));
+  isEdit = true;
+  isIndex = id;
+};
+
+// DeleteFunction
+const deleteRec = (id) => {
+    let data = [...Storage]
+
+    let deleteData = data.filter((delId) =>{
+        return delId.id != id 
+    })
+
+    localStorage.setItem("student_Data", JSON.stringify(deleteData));
+    Storage = getData();
+    displyData();
+}
+
+// displyDataFunftion
+const displyData = () => {
   recod.innerHTML = "";
 
   Storage.forEach((rec) => {
@@ -57,9 +143,9 @@ const addData = () => {
                             <td>${rec.age}</td>
                             <td>${rec.course}</td>
                             <td>
-                            <button class="btn btn-outline-primary">Edit</button>
-                            <button class="btn btn-outline-danger inline">Delete</button> 
+                            <button class="btn btn-outline-primary" onclick = "return singleRec(${rec.id})">Edit</button>
+                            <button class="btn btn-outline-danger inline" onclick ="return deleteRec(${rec.id})">Delete</button> 
                          </tr>`;
   });
 };
-addData();
+displyData();
